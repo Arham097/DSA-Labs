1a7a6840fa0dc93f4305b0037b9e91c7cd02acc6
Arham097 <arhamhasan70@gmail.com>
2024-01-03 20:19:04 +0500

    initial commit

--- tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-or(int i=0; i<n-1; i++){
-        int minindex = i;
-        for(int j=i+1; j<n; j++){
-            if(A[minindex]>A[j]){
-                minindex = j;
-            }
-        }
-        swap(A[i],A[minindex]);
-    }
